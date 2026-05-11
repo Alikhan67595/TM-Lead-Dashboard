d54d849098ef250d49ca0ef427d8fd4b6d7f58e1
Owais <www.owais664@gmail.com>
2026-05-11 10:49:04 +0500

    Delete function Update

--- src/Components/Leads.jsx
+++ src/Components/Leads.jsx
@@ -45,7 +45,7 @@ const Leads = () => {
   <>
     <table className='w-full flex flex-col'>
         <thead className='w-full'>
-            <tr className='w-full flex justify-around items-center py-2 border-black-50 border-y-2  bg-amber-300'>
+            <tr className='w-full flex justify-around items-center py-2 border-black-50 border-y-2  bg-amber-200'>
                 <th className='w-[3%]'>NO</th>
                 <th className='w-[10%]'>Serial</th>
                 <th className='w-[25%]'>Mark</th>
@@ -56,7 +56,7 @@ const Leads = () => {
                 <th className='w-[5%]'></th>
             </tr>
         </thead>
-    <tbody className='w-full'>
+    <tbody className='w-full flex flex-col'>
         {
            dataLeads?.length > 0 &&
             
@@ -66,7 +66,13 @@ const Leads = () => {
                                 ? lead.email.includes(lead.phone) 
                                 : false  
                 return(
-                <tr key={i+1} className={`${isPhoneInEmail ? "bg-red-200" : "bg-white"} group w-full flex justify-around items-center py-1 bg-gray-200  border-y-2 border-black-500`}>
+                <tr key={i+1} className={`${isPhoneInEmail ? "bg-red-200" : "bg-white"} group w-full flex justify-around items-center py-2 
+    border-y border-transparent 
+     hover:border-gray-200 
+    /* Left aur Bottom dono shadows mix kar di hain */
+    hover:shadow-[-5px_0_15px_-5px_rgba(0,0,0,0.1),0_4px_6px_-1px_rgba(0,0,0,0.1)]
+    hover:z-10 
+    transition-all duration-200 ease-in-out cursor-pointer`}>
                     <td className='text-center w-[3%] truncate overflow-hidden whitespace-nowrap'>{i+1}.</td>
                     <td className='text-center w-[10%] truncate overflow-hidden whitespace-nowrap'><a target='_blank' href={`https://tsdr.uspto.gov/#caseNumber=${lead.serial}&caseSearchType=CASE_SEARCH_NUMBER&caseType=DEFAULT&searchType=statusSearch`}>{lead.serial}</a></td>
                     <td className='text-center w-[25%] truncate overflow-hidden whitespace-nowrap'>{lead?.mark}</td>
